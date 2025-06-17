5be3ea072a302e67a6330d756f5b7816ed1e1f4e
book3039@knou.ac.kr <book3039@knou.ac.kr>
2025-06-17 10:25:06 -0400

    feat(data): add ErrorResponse and ErrorMessage classes for error handling

---
+++ core/data/src/main/java/com/example/data/model/ErrorMessage.kt
@@ -0,0 +1,6 @@
+package com.example.data.model
+
+data class ErrorMessage(
+    val code: Int,
+    val message: String
+)
--- core/data/src/main/java/com/example/data/response/ErrorResponse.kt
+++ core/data/src/main/java/com/example/data/response/ErrorResponse.kt
@@ -1,4 +1,12 @@
 package com.example.data.response
 
-class ErrorResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class ErrorResponse(
+    @SerializedName("errorCode")
+    val errorCode: String,
+    @SerializedName("errorMessage")
+    val errorMessage: String,
+    @SerializedName("errorFieldList")
+    val errorFiledList: List<String>,
+)
